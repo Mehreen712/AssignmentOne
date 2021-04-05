0cf25356568d6e74c736a9a3a478746fe127554f
Mehreen Tariq <mehreentariq028@gmail.com>
2021-04-05 10:48:25 -0700

    intent work

--- app/src/main/java/org/mehreen/alphabetsapp/MainActivity.java
+++ app/src/main/java/org/mehreen/alphabetsapp/MainActivity.java
@@ -2,13 +2,25 @@ package org.mehreen.alphabetsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+Button start;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = (Button) findViewById(R.id.startbutton);
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent Alphabet= new Intent (MainActivity.this,Alphabets.class);
+                startActivity(Alphabet);
+            }
+        });
+
+
     }
 }
